c4bb129a01b4a79ec46047e605ed2ed428af52d1
gribdesbois <gribdesbois@gmail.com>
2021-10-28 00:56:19 +0200

    added inline styles in JSX THERE BE DRAGON ==> specificity

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -52,7 +52,7 @@ const Book = () => {
 }
 const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="" />
 const Title = () => <h1>I Love You to the Moon and Back</h1>
-const Author = () => <h4>Amelia Hepworth</h4>
+const Author = () => <h4 style={{color:'#617d98', fontSize: '0.75rem', marginTop:'0.25rem'}}>Amelia Hepworth</h4>
 
 
 ReactDom.render(<BookList />, document.getElementById('root'))
