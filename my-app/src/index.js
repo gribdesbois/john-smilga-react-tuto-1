15b95a773e14c06d243aa129d8d2ba2bc31a0b89
gribdesbois <gribdesbois@gmail.com>
2021-10-28 00:36:06 +0200

    more books

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,6 +7,34 @@ function BookList() {
   return (
   <section>
     <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    
   </section>
   )
 
@@ -16,10 +44,14 @@ const Book = () => {
   return (
   <article>
     <Image />
+    <Title />
+    <Author />
   </article>
   )
 }
 const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="" />
+const Title = () => <h1>I Love You to the Moon and Back</h1>
+const Author = () => <h4>Amelia Hepworth</h4>
 
 
 ReactDom.render(<BookList />, document.getElementById('root'))
