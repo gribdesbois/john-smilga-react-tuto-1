0276bf2b6e0a75f7649160befcaa4b70e1b4be52
gribdesbois <gribdesbois@gmail.com>
2021-10-27 22:57:59 +0200

    typo

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDom from 'react-dom'
 
 function Greeting() {
   return <h4>this is Grib and this is one of my first components</h4>
 }
 
-ReactDOM.render(<Greeting />, document.getElementById('root'))
+ReactDom.render(<Greeting />, document.getElementById('root'))
