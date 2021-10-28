657630c2ae51972c8ce5e779ca7e3d688317b745
gribdesbois <gribdesbois@gmail.com>
2021-10-28 14:39:44 +0200

    events

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -39,11 +39,19 @@ function BookList() {
 
 const Book = (props) => {
   const {imgUrl, title, author} = props
+  //attribute, eventHandler
+  // onClick, onMouseHover
+  const clickHandler = () => {
+    alert('hello world')
+  }
   return (
   <article className="book">
     <img src={imgUrl} alt="" />
     <h1>{title}</h1>
     <h4>{author}</h4>
+    <button type="button" onClick={clickHandler}>
+      reference example
+    </button>
   </article>
   )
 }
