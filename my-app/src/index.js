66ec241a426a1ad6d804dcb7e8ff05b357218609
gribdesbois <gribdesbois@gmail.com>
2021-10-28 12:18:01 +0200

    adding expression in JSX

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,51 +8,22 @@ function BookList() {
   return (
   <section className="booklist">
     <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    
   </section>
   )
 
 }
-
+const author = 'Amelia Hepworth'
 const Book = () => {
+  const title = 'I Love You to the Moon and Back'
   return (
   <article className="book">
-    <Image />
-    <Title />
-    <Author />
+    <img src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="" />
+    <h1>{title}</h1>
+    <h4>{author.toUpperCase()}</h4>
+    {/* <p>{const  x = 6} </p> */}{/* Statements are prohibited */}
   </article>
   )
 }
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="" />
-const Title = () => <h1>I Love You to the Moon and Back</h1>
-const Author = () => <h4 style={{color:'#617d98', fontSize: '0.75rem', marginTop:'0.25rem'}}>Amelia Hepworth</h4>
 
 
 ReactDom.render(<BookList />, document.getElementById('root'))
