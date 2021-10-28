bc2422db48cd2e586111a49f9a5b1b95ea854eb4
gribdesbois <gribdesbois@gmail.com>
2021-10-28 14:06:46 +0200

    added key

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,14 +5,22 @@ import ReactDom from 'react-dom'
 import './index.css'
 const books = [
   {
+    id: 1,
     author: 'Amelia Hepworth',
     title: 'I Love You to the Moon and Back',
     imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'
   },
   {
+    id: 2,
     author: 'Dr Seuss',
     title: 'The Cat in the Hat in English and French',
     imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/516hUF87pTL._SX361_BO1,204,203,200_.jpg'
+  },
+  {
+    id: 3,
+    author: 'Brit Bennett',
+    title: 'The Vanishing Half: A Novel',
+    imgUrl: 'https://m.media-amazon.com/images/I/41ThX3SfAiL.jpg'
   }
 ]
 
@@ -20,9 +28,8 @@ function BookList() {
   return (
   <section className="booklist">
     {books.map((book)=> {
-      const {imgUrl, title, author} = book
       return (
-        <Book book={book}></Book>
+        <Book key={book.id} book={book}></Book>
       )
     })}
   </section>
